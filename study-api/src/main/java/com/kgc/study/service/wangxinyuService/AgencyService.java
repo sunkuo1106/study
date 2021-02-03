4e86ad82a1ba2081eabf415faee1299a2193e4fe
wangxinyu1008 <2401816735@qq.com>
2021-02-03 10:38:47 +0800

    王欣宇增加了机构查询

--- study-api/src/main/java/com/kgc/study/service/wangxinyuService/AgencyService.java
+++ study-api/src/main/java/com/kgc/study/service/wangxinyuService/AgencyService.java
@@ -1,8 +1,18 @@
 package com.kgc.study.service.wangxinyuService;
 
+import com.kgc.study.bean.AgencyAddress;
+import com.kgc.study.bean.AgencyInfo;
+
+import java.util.List;
+
 /**
  * @author 孙阔
  * @date 2021/2/2 14:39
  */
 public interface AgencyService {
+
+    //查询代理机构信息方法;
+    List<AgencyInfo> selectAgency(Long AgencyId);
+    //查询机构的所有上课地点信息方法
+    List<AgencyAddress> selectAllAgency(Long AgencyId);
 }
--- study-travel-coach/src/main/java/com/kgc/study/studytravelcoach/controller/wangxinyuController/AgencyController.java
+++ study-travel-coach/src/main/java/com/kgc/study/studytravelcoach/controller/wangxinyuController/AgencyController.java
@@ -1,7 +1,16 @@
 package com.kgc.study.studytravelcoach.controller.wangxinyuController;
 
+import com.kgc.study.bean.AgencyAddress;
+import com.kgc.study.bean.AgencyInfo;
+import com.kgc.study.service.wangxinyuService.AgencyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 孙阔
@@ -10,12 +19,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AgencyController {
-
+    @Resource
+    AgencyService agencyService;
     /*
     *跳转机构管理页面
     * */
     @GetMapping("/agency")
-    public String toAgency(){
+    public String toAgency(Model model, HttpSession session){
+        Integer agencyId = (Integer) session.getAttribute("AgencyId");
+        System.out.println(agencyId);
+        List<AgencyInfo> agencyInfos = agencyService.selectAgency(Long.valueOf(agencyId));
+        model.addAttribute("agencyInfo",agencyInfos);
+        List<AgencyAddress> agencyAddresses = agencyService.selectAllAgency(Long.valueOf(agencyId));
+        model.addAttribute("agencyAddresses",agencyAddresses);
         return "agency";
     }
 }
